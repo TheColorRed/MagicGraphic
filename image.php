e8de8f8ff648cda2e3e2f4a983859a44563d03e1
rnaddy <rnaddy@corp.acesse.com>
2013-12-25 10:27:33 -0600

    Added Cropping

--- image.php
+++ image.php
@@ -3,22 +3,26 @@
 require_once __DIR__ . "/src/MagicGraphic.php";
 header("content-type: image/jpeg");
 
+// Initialize a new Image
 $mg = new MagicGraphic();
 
+// Create the base layer from an image
 $sexy = $mg->createLayer("sexy");
 $sexy->loadFromFile("images/sexy-as-hell-31.jpg");
 $sexy->autoResizeWidth(500);
 
+// Create a second layer from an image
 $explosion = $mg->createLayer("expl1");
 $explosion->loadFromFile("images/explosion.png");
 $explosion->autoResizeWidth(150);
 $explosion->setOffset(100, 315);
 
+// Duplicate layer 2
 $explosion2 = $mg->duplicateLayer("expl2", $explosion->getData());
 $explosion2->setOffset(220, 350);
 $explosion2->rotate(90);
 $explosion2->crop(50, 50, 20, 20);
 
-
+// Crop the image and display
 $mg->crop(300, 300, 100, 200);
 $mg->display(0, MagicGraphic::PNG);
